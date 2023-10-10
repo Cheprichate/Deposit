003f4c9d0a49713aecacb3ad28ffec4951845e34
Cheprichate <petrovich15071998@mail.ru>
2023-10-10 11:13:16 +0400

    Добавьте файлы проекта.

---
+++ Deposit.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int perc ;
+int sSum ;
+int fSum ;
+int years ;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите сумму вклада\n";
+    std::cin >> sSum ;
+    std::cout << "Введите процентную ставку\n";
+    std::cin >> perc ;
+    std::cout << "Введите желаемую сумму\n";
+    std::cin >> fSum ;
+    while (sSum<fSum) {     
+        sSum+=(sSum / 100 * perc);
+        years++;  
+    }
+ std::cout << "Нужно подождать всего "<< years ;
+}
